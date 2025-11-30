5f2d2e76c511bfe25725f87268bd14c47f7655b1
saividyud <senthilnathan.11@osu.edu>
2025-11-30 02:01:31 -0500

    Created script files

---
+++ Trajectory/lambert_scanner_analysis.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+#SBATCH --time=6:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=1G
+#SBATCH --job-name=lambert_scanning_1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=senthilnathan.11@osu.edu
+
+#SBATCH --output="./Trajectory/lambert_scanner_analysis.out"
+
+# Commands to run
+module load mamba
+mamba activate .venv_space
+python ./Trajectory/lambert_scanner_analysis.py
